3e2bee710149003d2e51ecd48e53d893a672d604
Uttam_02 <mahatauttam0408@gmail.com>
2024-01-16 15:53:19 +0530

    debosmita's dataset integrated

--- rename_file.py
+++ rename_file.py
@@ -1,15 +1,17 @@
 import os
 
-count =16
 # Set the path to the directory containing the files
-directory_path = "Debosmita_Dataset/question1"
+directory_path = "Debosmita_Dataset/question4"
+
+# Set the starting count
+start_count = 16
 
 # Iterate through the files in the directory
-for count, filename in enumerate(os.listdir(directory_path)):
+for count, filename in enumerate(os.listdir(directory_path), start=start_count):
     # Check if the file is a .txt file and starts with "answer"
-    if filename.endswith(".txt") and filename.startswith("answer"):
+    if filename.endswith(".txt"):
         # Generate the new filename based on the count
-        new_filename = f"student+{count+1}_answer.txt"
+        new_filename = f"student{count}_answer.txt"
         
         # Construct the full paths for the old and new filenames
         old_filepath = os.path.join(directory_path, filename)
